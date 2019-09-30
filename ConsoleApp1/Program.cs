5c51587c0dcbb850549bb3ae47f0b55e37534bde
thanhan868192 <thanhan.lan@gmail.com>
2019-09-30 16:35:59 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Line");
+            int line = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input Row");
+            int row = int.Parse(Console.ReadLine());
+
+            for (int i = 0;i<10;i++)
+            {
+                for (int j = 0; j < 40; j++) ;
+                Console.WriteLine("*");
+            }
+
+        }
+    
+    }
+   
+}
